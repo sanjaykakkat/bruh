1cd0b5ebb59f45e11a268178db953d9d99c80f17
Sanjay <sungodnikaisme@gmail.com>
2026-08-08 21:56:58 +0530

    Modify money calculations and output balance
    Updated money variable calculations and console log output.

--- var.js
+++ var.js
@@ -3,7 +3,7 @@ let age = 18;
 let money = 2000; //lol should i also add my networth? it would add a 1 share of BOB
 const jsLearning = "yes";
 
-console.log (`Name: ${name}`);
-console.log (`Age: ${age}`);
-console.log (`Money: ${money}`);
-console.log (`Learning JavaScript ${jsLearning}`);
+let money -= 150;
+let money += 500;
+let money -=200;
+console.log (`The total balance on bank is ₹${money}`);
