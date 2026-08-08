a6b9dd57beff7792bad73dcb10b02bc98ef7db68
Sanjay <sungodnikaisme@gmail.com>
2026-08-08 21:47:32 +0530

    Add initial variables and console logs in var.js

---
+++ var.js
@@ -0,0 +1,9 @@
+const name = "Sanjay";
+let age = 18;
+let money = 2000; //lol should i also add my networth? it would add a 1 share of BOB
+const jsLearning = "yes";
+
+console.log (`Name: ${name}`);
+console.log (`Age: ${age}`);
+console.log (`Money: ${money}`);
+console.log (`Learning JavaScript ${jsLearning}`);
