c7784c2cde2f76b7f185f64c0a68482e2df3b6c3
Sanjay <sungodnikaisme@gmail.com>
2026-08-10 20:28:01 +0530

    Add age checks, ticket validation, and login logic

---
+++ idk.js
@@ -0,0 +1,45 @@
+// AGE
+let age = 18;
+
+if (age <= 13) {
+  alert ("You're freaking Minor bro😭");
+} else if (age <= 17) {
+  alert ("Okay atleast i wont be Epstein since you're a teenager");
+} else {
+  alert ("Hell yeah! can i get your number?");
+}
+
+// MOVIE TICKET
+
+let age = 18;
+let hasTicket = true;
+
+if (age >= 18 && hasTicket) {
+  alert ("Yeah buddy we're cool");
+} else {
+  alert ("Nah, hell nahh");
+}
+
+// DISCOUNT 
+
+let isStudent = true;
+let isSenior = false;
+
+if (isStudent || isSenior) {
+  alert ("Yeah you're getting a discount");
+} else {
+  alert ("Nah buddy!");
+}
+
+//LOGIN
+
+let inputUsername ='sanjay';
+let inputPassword ='123456';
+let username = "sanjay";
+let password = "123456";
+
+if ( inputusername === username && inputPassword === password ) {
+  alert ("logged in successfully");
+} else {
+  alert ("invalid username or password");
+}
