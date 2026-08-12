8ba33db291935dbc2d4bdd52dde1e02a9a549b75
Sanjay <sungodnikaisme@gmail.com>
2026-08-12 23:51:42 +0530

    Refactor loop.js to use console.log and clean up
    Updated alert statements to console.log for better debugging and removed commented-out code.

--- loop.js
+++ loop.js
@@ -1,19 +1,24 @@
-/*for (let i=1; i<=10; i++){
-  alert (i);
+for (let i=1; i<=10; i++){
+  console.log (i);
 }
-*/
+
 for (let b=10; b>=1; b = b - 1){
-  alert (b);
+  console.log (b);
 }
-/*
+
 for (let c=2; c<=20; c+= 2){
-  alert (c);
+  console.log (c);
 }
-he*/
+
 const clothes = ["shirt", "jeans", "shoes", "jacket"];
 
  
 for (clo = 0 clo<clothes.length; clo ++){
-  alert (clo);
+  console.log (clo);
 }
 
+const users = ["Alex", "Maya", "Sanjay", "John", "Lina"];
+
+for ( u=1; u<users.length; u++){
+  console.log (`users: ${u}`);
+}
