ff052e27e38c34e19c0e8a56eec94ccafa8c1e0c
Sanjay <sungodnikaisme@gmail.com>
2026-08-12 23:09:34 +0530

    Change alerts to console.log in loop.js
    Replaced alert with console.log for better debugging.

--- loop.js
+++ loop.js
@@ -1,13 +1,15 @@
 for (let i=1; i<=10; i++){
-  alert (i);
+  console.log(i);
 }
+
+console.log("hello");
 
 for (let b=10; b>1; b = b - 1){
   console.log(b);
 }
 
 for (let c=2; c<=20; c+= 2){
-  alert (c);
+  console.log(c);
 }
 
 const clothes = ["shirt", "jeans", "shoes", "jacket"];
