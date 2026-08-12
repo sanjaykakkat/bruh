6010e04290849be4a41ef03e4b211435f12d0f7f
Sanjay <sungodnikaisme@gmail.com>
2026-08-12 23:41:33 +0530

    Update var.js

--- var.js
+++ var.js
@@ -1,6 +1,6 @@
 const name = "Sanjay";
 let age = 18;
-let money = 2000; //lol should i also add my networth? it would add a 1 share of BOB
+let money = 2000; //lol, should i also add my networth? it would add a 1 share of BOB
 const jsLearning = "yes";
 
 let money -= 150;
