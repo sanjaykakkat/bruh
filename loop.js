deaca9b0786e172ec24a449fe8a43e74ecf545d0
Sanjay <sungodnikaisme@gmail.com>
2026-08-12 23:34:11 +0530

    Update loop conditions and clean up code
    Fixed loop conditions and removed commented code.

--- loop.js
+++ loop.js
@@ -1,19 +1,19 @@
-for (let i=1; i<=10; i++){
+/*for (let i=1; i<=10; i++){
   alert (i);
 }
-
-for (let b=10; b>1; b = b - 1){
+*/
+for (let b=10; b>=1; b = b - 1){
   alert (b);
 }
 /*
 for (let c=2; c<=20; c+= 2){
   alert (c);
 }
-
+*/
 const clothes = ["shirt", "jeans", "shoes", "jacket"];
 
  
 for (clo = 0 clo<clothes.length; clo ++){
   alert (clo);
 }
-*/
+
