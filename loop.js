92686be1141ac67c6d69120dc49067cfe8ec445c
Sanjay <sungodnikaisme@gmail.com>
2026-08-12 23:07:16 +0530

    Fix loop conditions and change alerts to console logs

--- loop.js
+++ loop.js
@@ -2,10 +2,17 @@ for (let i=1; i<=10; i++){
   alert (i);
 }
 
-for (let b=10; b<1; b = b - 1){
-  alert (b);
+for (let b=10; b>1; b = b - 1){
+  console.log(b);
 }
 
 for (let c=2; c<=20; c+= 2){
   alert (c);
+}
+
+const clothes = ["shirt", "jeans", "shoes", "jacket"];
+
+ 
+for (clo = clothes[0]; clo<= clothes[3]; clo ++){
+  console.log(clo);
 }
