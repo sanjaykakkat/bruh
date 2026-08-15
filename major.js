6d738dbb03eccd074b31f1cf6b71adc671000664
Sanjay <sungodnikaisme@gmail.com>
2026-08-15 12:51:08 +0530

    Update major.js

--- major.js
+++ major.js
@@ -9,3 +9,25 @@ function checkAge(age) {
 console.log(checkAge(15));
 console.log(checkAge(18));
 console.log(checkAge(25));
+
+// PART - 2
+
+function countTo(number) {
+    for (i=1; i<= number; i++){
+      console.log (i);
+    }
+}
+
+countTo(5);
+
+// PART - 3
+
+const clothes = ["shirt", "jeans", "shoes", "jacket"];
+
+function showClothes(items) {
+    for (i=0; i< clothes.length; i++) {
+      console.log (clothes[i]);
+    }
+}
+
+showClothes(clothes);
